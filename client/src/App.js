ace90361eff11fe75e317c304a171cbcdfdee481
Akshay Gudi <gudiakshay28@gmail.com>
2023-09-20 00:43:31 +0530

    react-bootrap add

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,29 @@
 
+import { useEffect } from 'react';
 import './App.css';
+const backend1 = "http://localhost:5000"
+const backend = "https://cookiee.onrender.com"
 
 function App() {
+
+
+  async function calltoback() {
+
+    const res = await fetch(backend, {
+      method: 'GET',
+      credentials: 'include'
+    });
+    const data = await res.json();
+    console.log(data);
+
+  }
+
+
+
   return (
-    <div className="App">
-      
+    <div className="App text-center text-danger">
+      <h1>Hello</h1>
+      <div className="btn btn-success" onClick={calltoback}>Click mann</div>
     </div>
   );
 }
